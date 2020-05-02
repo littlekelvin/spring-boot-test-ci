821826953326411e30155c183c59e1802be5b0b3
Kelvin Mai <kelvin.mai@oocl.com>
2020-05-02 23:04:58 +0800

    deploy

--- src/main/java/com/spring/test/ci/controller/AuthorController.java
+++ src/main/java/com/spring/test/ci/controller/AuthorController.java
@@ -4,6 +4,7 @@ import com.spring.test.ci.dto.ResponseDto;
 import com.spring.test.ci.entities.Author;
 import com.spring.test.ci.service.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,5 +33,10 @@ public class AuthorController {
         responseDto.setSuccess(true);
         responseDto.setData(authors);
         return  responseDto;
+    }
+
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable(value = "name") String name) {
+        return "hello, " + name;
     }
 }
