da09f9cb236bf9ce79c09f61c8d47b5d228d83a6
Kelvin Mai <kelvin.mai@oocl.com>
2020-05-02 23:30:33 +0800

    deploy

--- src/main/java/com/spring/test/ci/controller/AuthorController.java
+++ src/main/java/com/spring/test/ci/controller/AuthorController.java
@@ -37,6 +37,6 @@ public class AuthorController {
 
     @GetMapping("/hello/{name}")
     public String hello(@PathVariable(value = "name") String name) {
-        return "hello, " + name;
+        return "hello11, " + name;
     }
 }
